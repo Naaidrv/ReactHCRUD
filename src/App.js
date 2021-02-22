fce755a4051b4a4fd37808fd413981490cbd209f
Naaidrv <zabriverav@gmail.com>
2021-02-21 23:55:28 -0600

    Creación de CRUD con ReacJS Hooks

--- src/App.js
+++ src/App.js
@@ -1,24 +1,90 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid';
+import UserTable from './components/UserTable';
+import AddUserForm from './components/AddUserForm';
+import EditUserForm from './components/EditUserForm';
 
 function App() {
+
+  const userData = [
+    { id: uuidv4(), name: 'Tania', username: 'Butterfly'},
+    { id: uuidv4(), name: 'Craig', username: 'Mr. Developer'},
+    { id: uuidv4(), name: 'Ben', username: 'Beniphere'},
+  ] 
+
+  //Uso de State
+  const [users, setUsers] = useState(userData);
+
+  //Agregar usuario
+  const addUser = (user) => {
+    user.id = uuidv4()
+    setUsers([
+      ...users,
+      user
+    ])
+  }
+
+  // Eliminar Usuario
+  const deleteUser = (id) => {
+  //console.log(id)
+
+    const arrayFiltro = users.filter(user => user.id !== id);
+    setUsers(arrayFiltro);
+  }
+
+  //Editar Usuario
+  const [editing, setEditing] = useState(false);
+  const [currentUser, setCurrentUser] =useState({
+    id: null, name: '', username: ''
+  });
+
+  const editRow = (user) =>{
+    setEditing(true);
+    setCurrentUser({
+      id: user.id, name: user.name, username: user.username
+    })
+  }
+
+  //Actualizar datos
+
+  const updateUser = (id, updateUser) =>{
+    setEditing(false)
+    setUsers(users.map(user => (user.id === id ? updateUser : user)))
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+   <div className="container">
+     <h1>Aplicación CRUD con Hooks</h1>
+     <div className="flex-row">
+       <div className="flex-large">
+
+        {
+          editing ? (
+            <div> 
+            <h2>Editar Usuario</h2>
+            <EditUserForm
+            currentUser={currentUser}
+            updateUser={updateUser}/>
+            </div>
+          ) : (
+            <div>
+            <h2>Agregar Usuario</h2>
+            <AddUserForm addUser={addUser} />
+            </div>
+          )
+        }
+
+       </div>
+       <div className="flex-large">
+         <h2>Ver usuarios</h2>
+         <UserTable 
+         users={users} 
+         deleteUser={deleteUser}
+         editRow={editRow}
+         />
+         </div>
+     </div>
+   </div>
   );
 }
 
